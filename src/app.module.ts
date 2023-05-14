7b145f3468cf6085740dcac22c80ff6501c393f7
naylinn <naylinn.blueplanet@outlook.com>
2023-05-14 19:43:23 +0630

    conn between app and db

--- src/app.module.ts
+++ src/app.module.ts
@@ -1,11 +1,19 @@
 import { Module } from '@nestjs/common';
-import { AppController } from './app.controller';
-import { AppService } from './app.service';
+import { TypeOrmModule } from '@nestjs/typeorm';
+import { Url } from './url/url.entity';
 import { UrlModule } from './url/url.module';
 
 @Module({
-  imports: [UrlModule],
-  controllers: [AppController],
-  providers: [AppService],
+  imports: [
+    TypeOrmModule.forRoot({
+      type: 'sqlite',
+      database: 'URL.sqlite',
+      entities: [Url],
+      synchronize: true,
+    }),
+    UrlModule,
+  ],
+  controllers: [],
+  providers: [],
 })
 export class AppModule {}
