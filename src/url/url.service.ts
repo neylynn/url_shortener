80855f34b7de4958ee0c4326de310f930d3acc4c
naylinn <naylinn.blueplanet@outlook.com>
2023-05-14 19:52:59 +0630

    implement service logic

--- src/url/url.service.ts
+++ src/url/url.service.ts
@@ -1,4 +1,12 @@
 import { Injectable } from '@nestjs/common';
+import { Repository } from 'typeorm';
+import { InjectRepository } from '@nestjs/typeorm';
+import { Url } from './url.entity';
 
 @Injectable()
-export class UrlService {}
+export class UrlService {
+  constructor(
+    @InjectRepository(Url)
+    private repo: Repository<Url>,
+  ) {}
+}
